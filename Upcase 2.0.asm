b99af0dde39a4557a81964d9e4c68375fdd5911c
a13xanderplatz <a13xanderplatz10@outlook.com.ar>
2025-04-22 23:00:27 -0500

    Update Upcase 2.0.asm

--- Upcase 2.0.asm
+++ Upcase 2.0.asm
@@ -12,7 +12,7 @@ __start:
     # Inicialización
     la $s0, orig          # Dirección de la cadena
     li $t0, 0             # Contador de posición
-    li $t2, 1             # Flag: 1 = próximo carácter debe verificarse (primer carácter o después de espacio)
+    li $t2, 1             # Flag: 1 = próximo carácter debe verificarse
     li $t6, 0x20          # ASCII space
     li $t7, 0x61          # ASCII 'a'
     li $t8, 0x7a          # ASCII 'z'
@@ -25,21 +25,37 @@ loop:
     # Verificar si es primer carácter o después de espacio
     beq $t2, $zero, check_space
     
-    # Verificar si es minúscula (a-z)
+    # Verificar si es minúscula (a-z o á,é,í,ó,ú,ñ)
+    # Primero, rango a-z
     slt $t3, $t1, $t7     # t3=1 si t1 < 'a'
-    slt $t4, $t8, $t1      # t4=1 si t1 > 'z'
-    or $t3, $t3, $t4       # t3=1 si fuera de a-z
-    bne $t3, $zero, not_lower
+    slt $t4, $t8, $t1     # t4=1 si t1 > 'z'
+    or $t3, $t3, $t4      # t3=1 si fuera de a-z
+    beq $t3, $zero, is_lower  # Si está en a-z, convertir
     
-    # Convertir a mayúscula
-    addi $t1, $t1, -32    # Diferencia ASCII entre may/min
+    # Verificar caracteres con tildes y ñ
+    li $t5, 0xE1          # á
+    beq $t1, $t5, is_accent
+    li $t5, 0xE9          # é
+    beq $t1, $t5, is_accent
+    li $t5, 0xED          # í
+    beq $t1, $t5, is_accent
+    li $t5, 0xF3          # ó
+    beq $t1, $t5, is_accent
+    li $t5, 0xFA          # ú
+    beq $t1, $t5, is_accent
+    li $t5, 0xF1          # ñ
+    beq $t1, $t5, is_accent
+    j not_lower           # No es minúscula ni acentuada
+
+is_accent:
+is_lower:
+    addi $t1, $t1, -32    # Convertir a mayúscula (válido para a-z y á,é,í,ó,ú,ñ)
     sb $t1, 0($t0)        # Guardar carácter modificado
     
 not_lower:
     li $t2, 0             # Resetear flag
     
 check_space:
-    # Verificar si es espacio para marcar próximo carácter
     bne $t1, $t6, not_space
     li $t2, 1             # Marcar que próximo carácter debe verificarse
     
@@ -61,6 +77,6 @@ endLoop:
     syscall
 
 .data
-orig: .asciiz "la cadena original con letras todas minusculas"
+orig: .asciiz "la canción, auténtica y única"  # Cadena con tildes
 prm1: .asciiz "Original: "
 prm2: .asciiz "\nUpcased : "
